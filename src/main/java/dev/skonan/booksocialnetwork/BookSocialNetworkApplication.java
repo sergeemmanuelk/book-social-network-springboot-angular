534300ccdc7c348cf30d6d48671572e1e9dc7417
sergeemmanuelk <serge_em@outlook.fr>
2024-04-23 23:17:02 +0000

    project setup

---
+++ src/main/java/dev/skonan/booksocialnetwork/BookSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package dev.skonan.booksocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSocialNetworkApplication.class, args);
+	}
+
+}
