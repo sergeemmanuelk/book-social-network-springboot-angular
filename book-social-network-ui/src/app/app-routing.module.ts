a275b824c6720e06f3d31f1ee5d978287c29c43a
sergeemmanuelk <serge_em@outlook.fr>
2024-08-26 21:07:31 +0000

    secure routes with authentication guard

--- book-social-network-ui/src/app/app-routing.module.ts
+++ book-social-network-ui/src/app/app-routing.module.ts
@@ -3,28 +3,31 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
+import { authGuard } from './services/guard/auth.guard';
 
 const routes: Routes = [
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
   },
   {
     path: 'register',
-    component: RegisterComponent
+    component: RegisterComponent,
   },
   {
     path: 'activate-account',
-    component: ActivateAccountComponent
+    component: ActivateAccountComponent,
   },
   {
     path: 'books',
-    loadChildren: () => import('./modules/book/book.module').then(m => m.BookModule)
-  }
+    loadChildren: () =>
+      import('./modules/book/book.module').then((m) => m.BookModule),
+    canActivate: [authGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- book-social-network-ui/src/app/modules/book/book-routing.module.ts
+++ book-social-network-ui/src/app/modules/book/book-routing.module.ts
@@ -6,35 +6,43 @@ import { MyBooksComponent } from './pages/my-books/my-books.component';
 import { ManageBookComponent } from './pages/manage-book/manage-book.component';
 import { BorrowedBookListComponent } from './pages/borrowed-book-list/borrowed-book-list.component';
 import { ReturnBookComponent } from './pages/return-book/return-book.component';
+import { authGuard } from 'src/app/services/guard/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
+    canActivate: [authGuard],
     children: [
       {
         path: '',
-        component: BookListComponent
+        component: BookListComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'my-books',
-        component: MyBooksComponent
+        component: MyBooksComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'my-borrowed-books',
-        component: BorrowedBookListComponent
+        component: BorrowedBookListComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'my-returned-books',
-        component: ReturnBookComponent
+        component: ReturnBookComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'manage',
-        component: ManageBookComponent
+        component: ManageBookComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'manage/:bookId',
-        component: ManageBookComponent
+        component: ManageBookComponent,
+        canActivate: [authGuard]
       }
     ]
   }
